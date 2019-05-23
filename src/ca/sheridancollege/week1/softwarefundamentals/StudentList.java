1c55f295eb8b22dbfcafb0a285f285a0be0fe35f
nirmannat <natn@sheridancollege.ca>
2019-05-23 12:36:12 -0400

    Committing

--- src/ca/sheridancollege/week1/softwarefundamentals/StudentList.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/StudentList.java
@@ -24,6 +24,7 @@ public class StudentList {
         for (int i=0;i<sList.length;i++)
         {
             System.out.println(sList[i].getName());
+            //Hello
           
         }
         
